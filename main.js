94a06f0f3f41c82d1cc500040fa1a04f1048e544
Dominika <dominika.jansova1011@gmail.com>
2021-03-24 15:12:29 +0100

    function saveJoke

--- main.js
+++ main.js
@@ -1,23 +1,52 @@
-const $joke = document.getElementById('joke-new-text');
+const $newJoke = document.getElementById("joke-new-text");
+const $columnsLeft = document.getElementById("columns-left");
 
-const $getJoke = document.getElementById('btn-another');
-const $saveJoke = document.getElementById('btn-save');
-cosnt $deleteJoke = document.getElementById('btn-delete');
+const $getJoke = document.getElementById("btn-another");
+const $saveJoke = document.getElementById("btn-save");
 
 // get joke API
+
 getJoke();
 
 async function getJoke() {
-    const getData = await fetch('https://icanhazdadjoke.com/', {
-        headers: {
-            'Accept': 'application/json'
-        }
-    })
-    const joke = await getData.json();
-    $joke.innerHTML = joke.joke;
+  const jokeResponse = await fetch("https://icanhazdadjoke.com/", {
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const joke = await jokeResponse.json();
+  $newJoke.innerHTML = joke.joke;
 }
 
 $getJoke.onclick = getJoke;
 
+// add joke
+$saveJoke.addEventListener("click", saveJoke);
+
+function saveJoke(event) {
+  const $jokeItem = document.createElement("div");
+  $jokeItem.classList.add("joke-item");
+
+  const $jokeText = document.createElement("p");
+  $jokeText.innerText = $newJoke.innerText;
+  $jokeText.classList.add("joke-text");
+  $jokeItem.appendChild($jokeText);
+
+  const $jokeDelete = document.createElement("button");
+  $jokeDelete.innerHTML = "delete";
+  $jokeDelete.classList.add("btn-delete");
+  $jokeItem.appendChild($jokeDelete);
+
+  $columnsLeft.appendChild($jokeItem);
+}
+
 // delete joke
 
+// function deleteJoke() {
+//     const $jokeItem = document.getElementById('joke-item');
+//     const parent = $jokeItem.parentNode;
+//     parent.removeChild($jokeItem);
+//     console.log('should be removed')
+// }
+
+// $jokeDelete.onclick = deleteJoke;
