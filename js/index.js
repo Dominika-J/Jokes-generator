adb2693a96fbf5b92a1a116f43bec888372f425c
Dominika <dominika.jansova1011@gmail.com>
2021-04-01 16:21:41 +0200

    code clean-up 3

--- js/index.js
+++ js/index.js
@@ -1,8 +1,7 @@
 import { setCookie, getCookie } from './cookies.js'
+import { saveJoke, deleteJoke } from './jokes.js'
 
 const $newJoke = document.getElementById("joke-new-text");
-const $columnsLeft = document.getElementById("columns-left");
-
 const $getJoke = document.getElementById("btn-another");
 const $saveJoke = document.getElementById("btn-save");
 
@@ -35,40 +34,6 @@ $saveJoke.addEventListener("click", () => {
 });
 
 console.log(getCookie('jokeIds'));
-
-// create joke
-
-function saveJoke() {
-  const $jokeItem = document.createElement("div"); // created div 
-  $jokeItem.classList.add("joke-item");
-
-  const $jokeText = document.createElement("p"); // created p
-  $jokeText.innerText = $newJoke.innerText; //parametr
-  $jokeText.classList.add("joke-text");
-  $jokeItem.appendChild($jokeText);
-
-  const $jokeDelete = document.createElement("button"); // created button
-  $jokeDelete.innerHTML = "Delete";
-  $jokeDelete.classList.add("btn-delete");
-  const $jokeTrash = document.createElement("i"); // created button icon
-  $jokeTrash.className = "far fa-trash-alt fa"; 
-
-  $jokeDelete.appendChild($jokeTrash);
-  $jokeItem.appendChild($jokeDelete);
-  $columnsLeft.appendChild($jokeItem);
-
-  $jokeDelete.addEventListener("click", deleteJoke); 
-}
-
-// delete joke
-
-function deleteJoke(e) {
-    const item = e.target;
-    if(item.classList[0] === "btn-delete") {
-        const $jokeItem = item.parentElement;
-        $jokeItem.remove();
-    }
-}
 
 // fetch joke
 
---
+++ js/jokes.js
@@ -0,0 +1,36 @@
+const $columnsLeft = document.getElementById("columns-left");
+
+
+// create joke
+
+export function saveJoke() {
+    const $jokeItem = document.createElement("div"); // created div 
+    $jokeItem.classList.add("joke-item");
+  
+    const $jokeText = document.createElement("p"); // created p
+    $jokeText.innerText = $newJoke.innerText; //parametr
+    $jokeText.classList.add("joke-text");
+    $jokeItem.appendChild($jokeText);
+  
+    const $jokeDelete = document.createElement("button"); // created button
+    $jokeDelete.innerHTML = "Delete";
+    $jokeDelete.classList.add("btn-delete");
+    const $jokeTrash = document.createElement("i"); // created button icon
+    $jokeTrash.className = "far fa-trash-alt fa"; 
+  
+    $jokeDelete.appendChild($jokeTrash);
+    $jokeItem.appendChild($jokeDelete);
+    $columnsLeft.appendChild($jokeItem);
+  
+    $jokeDelete.addEventListener("click", deleteJoke); 
+  }
+  
+  // delete joke
+  
+  export function deleteJoke(e) {
+      const item = e.target;
+      if(item.classList[0] === "btn-delete") {
+          const $jokeItem = item.parentElement;
+          $jokeItem.remove();
+      }
+  }
